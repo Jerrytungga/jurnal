0bc34376076b03cadde036dc3a501a8d907f34f0
Jerry Christian <jerrychristiangedeontungga41@gmail.com>
2021-11-13 17:07:12 +0700

    paesan

--- mentor/modal/function.php
+++ mentor/modal/function.php
@@ -1,6 +1,6 @@
 <?php
 // menghubungkan ke database
-$conn = mysqli_connect("127.0.0.1", "root", "", "jurnal");
+$conn = mysqli_connect("127.0.0.1", "root", "#dbabsensipka#", "jurnal");
 // menampilkan data siswa
 function query($query)
 {
