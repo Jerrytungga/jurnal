d2ba7e6ed752805ae3ce3e3545b38254075bd28d
Jerry Christian <jerrychristiangedeontungga41@gmail.com>
2021-12-07 23:46:58 +0700

    pesan

--- siswa/template/topbar_menu.php
+++ siswa/template/topbar_menu.php
@@ -1,5 +1,5 @@
 <ul class="navbar-nav ml-auto">
-    <div class="topbar-divider d-none d-sm-block"></div>
+
 
     <!-- Nav Item - User Information -->
     <li class="nav-item dropdown no-arrow">
@@ -11,7 +11,6 @@
         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
 
 
-            <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                 Logout
