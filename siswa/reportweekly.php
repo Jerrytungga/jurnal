147a016a0ddc4a7c5fecb03f32a991210fc107f8
jerrychristiangedeontungga41@gmail.com <jerrychristiangedeontungga41@gmail.com>
2022-06-07 23:09:48 +0700

    pesan

--- siswa/reportweekly.php
+++ siswa/reportweekly.php
@@ -50,9 +50,6 @@ include 'template/head.php'
                     </div>
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4 ">
-                        <div class="card-header py-3">
-                            <h6 class=" font-weight-bold text-dark">Laporan Mingguan</h6>
-                        </div>
                         <div class="card-body">
                             <div class="table-responsive">
                                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
